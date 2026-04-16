ca4ff6c457786764b22c15a90c5f6f2410f9a907
Aluno <aluno.ipora@ifgoiano.local>
2026-04-16 15:48:34 -0300

    Estrutura

--- exe1.c
+++ exe1.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <stdbool.h>
+
+typedef struct No{
+    char valor;
+    struct No *prox;
+}No;
+
+typedef struct Fila{
+    struct No *inicio;
+    struct No *fim;
+}Fila;
+
+Fila *criarFila(){
+    Fila *f = malloc(sizeof(Fila));
+    f->inicio = NULL;
+    f->fim = NULL;
+}
+
+bool isEmptyFila(Fila *f){
+    return f->inicio == NULL;
+
+}
+
+int inserir(Fila *f, char valor){
+    if(isEmptyFila(f)){
+        printf("\nFila vazia\n");
+        return 0;
+    }
+}
 
 int main(){
-    printf("Hello, world");
+
 
 return 0;}
